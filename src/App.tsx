3cca0da9dadf6aef20f4d8ea4a53007f3cb3796a
Alla-Nazarenko <umnyj.start@gmail.com>
2025-01-24 09:44:16 +0100

    homework 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import "./App.css";
 
 //lessons
-import Lesson06 from "./lessons/lesson06/Lesson06";
+// import Lesson06 from "./lessons/lesson06/Lesson06";
 
 //homeworks
 
 //consultations
+import Consultation03 from "./consultations/consultatin03/Consultation03";
 
 function App() {
   return (
     <>
       <div className="app">
         {/* --------------Topic: TypeScript Introduction */}
-        <Lesson06 />
+        {/* <Lesson06 /> */}
+        <Consultation03 />
       </div>
     </>
   );
