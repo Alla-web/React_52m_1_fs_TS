2dcee495269ff06970ad54d677af66353206fb24
Alla-Nazarenko <umnyj.start@gmail.com>
2025-02-03 14:27:11 +0100

    lesson 13: added comment

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -7,7 +7,8 @@ function Users() {
   return (
     <UsersPage>
       <Title>Select user</Title>
-      т.к. мы на странице, с которой будем переходить на user, то в to
+      {/* т.к. мы на странице, с которой будем переходить на user, 
+      то в to можно писать только целевую страницу*/}
       <Link to='user'>Go to User page</Link>
       <Button name="Go to About Page" onClick={()=>{}} />
     </UsersPage>
