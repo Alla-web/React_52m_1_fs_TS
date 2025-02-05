43f0df6e2cb12f9e72fcda65ada150db3b16a7a3
Alla-Nazarenko <umnyj.start@gmail.com>
2025-02-05 10:20:59 +0100

    added conditionsl rendering

--- src/homeworks/Homework14/components/Card/Card.tsx
+++ src/homeworks/Homework14/components/Card/Card.tsx
@@ -1,9 +1,14 @@
 import { CardContainer, Name } from "./styles";
 import Message from "../Message/Message";
+import { useContext } from "react";
+import { MessageContext } from "../BlogManagement/BlogManagement";
 
 function Card() {
+   //принимаем значение в useContext из MessageContext в BlogManagement
+    const isMessage = useContext(MessageContext);
   return (
-    <CardContainer>
+    //посмотреть как у Катерины через пропс
+    (!!isMessage) && <CardContainer>
       <Name>Alla Nazarenko</Name>
       <Message/>
     </CardContainer>
--- src/homeworks/Homework14/components/Message/Message.tsx
+++ src/homeworks/Homework14/components/Message/Message.tsx
@@ -2,14 +2,14 @@ import { useContext } from "react";
 import { MessageContainer, MessageItem } from "./styles";
 import { MessageContext } from "../BlogManagement/BlogManagement";
 
-function Message (){
-
-   const message = useContext(MessageContext);
-    return(
-        <MessageContainer>
-            <MessageItem>{message}</MessageItem>
-        </MessageContainer>
-    );
+function Message() {
+  //принимаем значение в useContext из MessageContext в BlogManagement
+  const message = useContext(MessageContext);
+  return (
+    <MessageContainer>
+      <MessageItem>{message}</MessageItem>
+    </MessageContainer>
+  );
 }
 
 export default Message;
